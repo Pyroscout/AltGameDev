e37a54c72b26f7d5bdba39ddc6b7ffb58666f2af
Pyroscout <pyroscout14@gmail.com>
2018-02-25 15:22:30 -0700

    replace obsolete method

--- Assets/Scripts/Menu Actions/MenuActions.cs
+++ Assets/Scripts/Menu Actions/MenuActions.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
+
 public class MenuActions : MonoBehaviour
 {
     public void MENU_ACTION_GotoPage(string sceneName)
     {
-        Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
