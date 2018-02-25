ea8308f08f08a1001f6bd6e74b2ad13e2c904bf3
Michael Caldwell <slowinfect@gmail.com>
2018-02-24 18:32:39 -0700

    Menu Scripting and Evolution Screen
    Start of UI assets
    Coding to switch between Scenes
    Added fonts

---
+++ Assets/Scripts/Menu Actions/MenuActions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MenuActions : MonoBehaviour
+{
+    public void MENU_ACTION_GotoPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
