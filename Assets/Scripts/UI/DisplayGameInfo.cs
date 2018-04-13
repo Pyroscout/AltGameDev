9d1387af18e2fc019fb2aa36b67740104d4b3cb7
Amedes95 <pielord95@gmail.com>
2018-04-13 17:28:40 -0600

    temp push

---
+++ Assets/Scripts/UI/DisplayGameInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayGameInfo : MonoBehaviour
+{
+    public GameObject InfoCanvas;
+
+	// Use this for initialization
+	public void show ()
+    {
+        InfoCanvas.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	public void hide ()
+    {
+        InfoCanvas.SetActive(false);
+	}
+}
