17100210742d5c036f7929ab3e3bef41b7d08dd6
Amedes95 <pielord95@gmail.com>
2018-04-14 00:12:57 -0600

    menu

--- Assets/Scripts/UI/DisplayGameInfo.cs
+++ Assets/Scripts/UI/DisplayGameInfo.cs
@@ -4,16 +4,25 @@ using UnityEngine.UI;
 public class DisplayGameInfo : MonoBehaviour
 {
     public GameObject InfoCanvas;
+    public GameObject wing1;
+    public GameObject wing2;
+    public GameObject totem;
 
 	// Use this for initialization
 	public void show ()
     {
+        totem.SetActive(false);
+        wing1.SetActive(false);
+        wing2.SetActive(false);
         InfoCanvas.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	public void hide ()
     {
+        wing1.SetActive(true);
+        wing2.SetActive(true);
+        totem.SetActive(true);
         InfoCanvas.SetActive(false);
 	}
 }
