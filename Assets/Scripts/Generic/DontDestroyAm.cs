84db74075cd28a381a83d3e2917bd64d09432a49
Michael Caldwell <slowinfect@gmail.com>
2018-04-15 15:46:56 -0600

    Music and Ambiance Added

---
+++ Assets/Scripts/Generic/DontDestroyAm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DontDestroyAm: MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameObject[] objs2 = GameObject.FindGameObjectsWithTag("Ambiance");
+        if (objs2.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
